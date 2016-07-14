95e79c7c555cb2012bf6ca9e034d28dc8fe52a86
AVVS <v@aminev.me>
2016-07-14 03:37:13 +0300

    refactor: add more variables, dont spam console

--- readAndChangeXML.py
+++ readAndChangeXML.py
@@ -1,16 +1,31 @@
+#!/usr/bin/env python
 import xml.etree.cElementTree as ET
 import urllib2
 import json
+import time
+import sys
+import random
+import signal
 
+ip = "http://192.168.1.3:8080/"
 lastLat = ""
 lastLng = ""
 
+def id_generator(size=1, chars="\\|/"):
+   return ''.join(random.choice(chars) for _ in range(size))
+
+def signal_handler(signal, frame):
+    print('\nClosing app!')
+    sys.exit(0)
+
 def getPokemonLocation():
 	try:
-		response = urllib2.urlopen("http://172.16.255.195/", timeout = 1)
+		response = urllib2.urlopen(ip, timeout = 1)
 		return json.load(response)
 	except urllib2.URLError as e:
-		print e.reason
+		print id_generator(), "Error:", e.reason, "                            \r",
+		sys.stdout.flush()
+		time.sleep(1)
 
 def generateXML():
 	global lastLat, lastLng
@@ -23,10 +38,13 @@ def generateXML():
 			wpt = ET.SubElement(gpx, "wpt", lat=geo["lat"], lon=geo["lng"])
 			ET.SubElement(wpt, "name").text = "PokemonLocation"
 			ET.ElementTree(gpx).write("pokemonLocation.gpx")
-			print "Location Updated!", "latitude:", geo["lat"], "longitude:" ,geo["lng"]
+			print id_generator(), "Location Updated!", "latitude:", geo["lat"], "longitude:" ,geo["lng"], "\r",
+			sys.stdout.flush()
 
 def start():
 	while True:
 		generateXML()
+
+signal.signal(signal.SIGINT, signal_handler)
 
 start()
--- runAutoClicker.py
+++ runAutoClicker.py
@@ -1,24 +1,42 @@
+#!/usr/bin/env python
 import os
 import urllib2
 import json
 import time
+import sys
+import signal
+import random
+
+ip = "http://192.168.1.3:8080/"
+
+def signal_handler(signal, frame):
+    print('\nClosing app!')
+    sys.exit(0)
 
 def checkConnected():
 	try:
-		response = urllib2.urlopen("http://172.16.255.195/", timeout = 1)
+		response = urllib2.urlopen(ip, timeout = 1)
 		return json.load(response)
 	except urllib2.URLError as e:
-		print e.reason
+		print id_generator(), "Error:", e.reason, "                            \r",
+		sys.stdout.flush()
+		time.sleep(1)
+
+def id_generator(size=1, chars="\\|/"):
+   return ''.join(random.choice(chars) for _ in range(size))
 
 def clickAction():
-	os.system("./autoClicker -x 750 -y 400")
-	os.system("./autoClicker -x 750 -y 450")
+	os.system("./autoClicker -x 505 -y 1049")
+	os.system("./autoClicker -x 544 -y 1103")
 	time.sleep(1)
-	print "clicking!!"
+	print id_generator(), "clicking!!              \r",
+	sys.stdout.flush()
 
 def start():
 	while True:
 		if checkConnected() != None:
 			clickAction()
+
+signal.signal(signal.SIGINT, signal_handler)
 
 start()
